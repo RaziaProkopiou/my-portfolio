71009f71888304b39c9f480726411f20cead2c58
Razia Prokopiou <razia@al.co.za>
2025-04-01 19:05:34 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind! 🎉</h1>
+    </div>
+  );
+}
